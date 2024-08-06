4901e9264e93d51b6559290105d80264a7e9b85f
ibnumanao <ibnuafandi156@yahoo.com>
2024-08-06 15:43:38 +0700

    option menu1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Tombol jadi Merah", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater Inflater= getMenuInflater();
+        Inflater.inflate(R.menu.optionmenu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu1){
+            startActivity(new Intent(this, menu1.class));
+        }
+        else if (item.getItemId()==R.id.menu2) {
+            startActivity(new Intent(this, menu2.class));
+        } else if (item.getItemId()==R.id.menu3) {
+            startActivity(new Intent(this, menu3.class));
+        }
+
+
+        return true;
     }
 }
